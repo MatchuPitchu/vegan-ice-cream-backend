cfa039b04c24787b18f30d0679fe54f4172e9d07
Matchu Pitchu <77695211+MatchuPitchu@users.noreply.github.com>
2021-04-29 19:19:04 +0200

    Add comment

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,8 @@ const userRouter = express.Router();
 
 userRouter.get('/', getAllUsers);
 userRouter.get('/:id', getSingleUser);
+
+// QUESTION: NEED TO PROTECT THIS WITH MIDDLEWARE verifyToken? OR CAN I SET SOME ROUTES LATER PRIVATE ON HEROKU?
 userRouter.get('/:id/infos', getAllInfosFromUser);
 
 export default userRouter;
