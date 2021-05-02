0fe18e11b44fd5e6641214f4ad2d1c93d8a41fdf
Matchu Pitchu <77695211+MatchuPitchu@users.noreply.github.com>
2021-05-02 19:01:11 +0200

    Add useCreateIndex = true

--- db/database.js
+++ db/database.js
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 mongoose.connect(process.env.MONGO_URI, { 
   useNewUrlParser: true, 
   useUnifiedTopology: true, 
-  useFindAndModify: false 
+  useFindAndModify: false,
+  useCreateIndex: true
 });
 
 // If MongoDB connection err
